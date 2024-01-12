01228b6b429195e1fb3a6bb3cd706364d9b9a668
Abhijit Das <abhijitd721201@gmail.com>
2024-01-12 11:40:07 +0530

    two dimensional and complex array

--- Numpy/02_type_of_array.py
+++ Numpy/02_type_of_array.py
@@ -2,3 +2,11 @@
 import numpy as np 
 a=np.array([1,2,3,4,5,6])
 print(a)
+
+# two dimensional array
+a1=np.array([[1,2,3] , [1,2,3]])
+print(a1)
+
+# complex type
+a3=np.array([1,2,3] , "complex")
+print(a3)           # output => [1.+0.j 2.+0.j 3.+0.j]
