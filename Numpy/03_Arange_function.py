ccbfd7e652c84e039592d3fa25a9029c0e97e715
Abhijit Das <abhijitd721201@gmail.com>
2024-01-12 11:50:58 +0530

    first commit

---
+++ Numpy/03_Arange_function.py
@@ -0,0 +1,6 @@
+# Arange fucntion is use like range()
+# it is use to create a array the mention in range function
+
+import numpy as np 
+a=np.arange(1,11)
+print(a)        # output=> [ 1  2  3  4  5  6  7  8  9 10]
