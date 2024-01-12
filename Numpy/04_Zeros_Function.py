d6eafa5e40276d533cd09c57d215bccbdf12cfb7
Abhijit Das <abhijitd721201@gmail.com>
2024-01-12 12:00:04 +0530

    first commit

---
+++ Numpy/04_Zeros_Function.py
@@ -0,0 +1,11 @@
+# zeros() use to create array with zero filled..
+# its default datatype is zero
+
+import numpy as np 
+
+a=np.zeros(5)       # we have to mention the shape of array
+print(a)        # [0. 0. 0. 0. 0.]
+
+# but we can change the datatype
+a=np.zeros(5 , dtype=int)
+print(a)        # output-> [0 0 0 0 0]
